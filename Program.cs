fa825fe72845684c5a60a519e0759c4dacf03410
Abrar Jahin <abrar.jahin.2652@gmail.com>
2020-12-14 17:56:34 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AltitiudeChange
+{
+    /// <summary>
+    /// Altitude Change
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Prompt for and getting altitude.
+        /// Calculate and display the altitude change
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first altitude: ");
+            int firstAltitude = int.Parse(Console.ReadLine());
+            Console.Write("Enter second altitude: ");
+            int secondAltitude = int.Parse(Console.ReadLine());
+            Console.Write("Change in altitudes: " + (secondAltitude - firstAltitude));
+        }
+    }
+}
